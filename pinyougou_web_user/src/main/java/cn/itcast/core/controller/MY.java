53dc03840954e3634cf69b80e1e17c16334b5573
zxy <3132884662@qq.com>
2019-03-31 13:00:39 +0800

    12121

--- pinyougou_web_user/src/main/java/cn/itcast/core/controller/MY.java
+++ pinyougou_web_user/src/main/java/cn/itcast/core/controller/MY.java
@@ -2,6 +2,6 @@ package cn.itcast.core.controller;
 
 public class MY {
     public static void main(String[] args) {
-        System.out.println("许强做了修改");
+        System.out.println("许强做了修改hjkh");
     }
 }
